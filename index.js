7dd10107e1947c8b27273df3bb8fda862a76bf2a
hidehidev7 <hidehidev7@gmail.com>
2025-03-21 21:15:27 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import http from 'node:http';
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('dvdrental', 'postgres', 'matrix_revolution', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+});
+server.listen(3000);
