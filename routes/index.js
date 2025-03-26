275b85d96fdf8064862633a0472c1206d6eb7d26
hidehidev7 <hidehidev7@gmail.com>
2025-03-26 22:27:46 +0900

    modify: apply 1sec setTimeout for 'serverbutton/push'

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 router.get('/serverbutton/', function(req, res, next) {
-  res.render('serverbutton');
+    res.render('serverbutton');
 });
 
 router.get('/serverbutton/push/', function(req, res, next) {
-  res.render('serverbutton');
+  setTimeout(() => {
+    res.render('serverbutton');
+  }, 1000);
 });
 
 module.exports = router;
