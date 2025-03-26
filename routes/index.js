98a8eded20f69f4b23c5276d07337d7b37d40888
hidehidev7 <hidehidev7@gmail.com>
2025-03-27 08:59:31 +0900

    modify: route 'serverbutton/push': req from same IP will be ignored for 1sec

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,15 @@ router.get('/serverbutton/', function(req, res, next) {
   res.render('serverbutton', { serverButtonCount: app.data.serverButtonCount });
 });
 
+let serverButtonDenyIPList = [];
+
 router.get('/serverbutton/push/', function(req, res, next) {
+  const ip = req.ip;
+  if(serverButtonDenyIPList.includes(ip)) return;
+  else serverButtonDenyIPList.push(ip);
   app.data.serverButtonCount++;
   setTimeout(() => {
+    serverButtonDenyIPList.splice(serverButtonDenyIPList.indexOf(ip), 1);
     res.redirect('/serverbutton/');
   }, 1000);
 });
