433cb03bb4914394a4ea22a6d520c1c03960786f
hidehidev7 <hidehidev7@gmail.com>
2025-03-27 09:07:33 +0900

    fix: the req won't be ignored but don't increase buttoncount on route '/serverbutton/push'

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,26 @@ router.setApp = a => {
 }
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-router.get('/serverbutton/', function(req, res, next) {
+router.get('/serverbutton/', function (req, res, next) {
   res.render('serverbutton', { serverButtonCount: app.data.serverButtonCount });
 });
 
 let serverButtonDenyIPList = [];
 
-router.get('/serverbutton/push/', function(req, res, next) {
+router.get('/serverbutton/push/', function (req, res, next) {
   const ip = req.ip;
-  if(serverButtonDenyIPList.includes(ip)) return;
-  else serverButtonDenyIPList.push(ip);
-  app.data.serverButtonCount++;
+  if (serverButtonDenyIPList.includes(ip)) { }
+  else {
+    serverButtonDenyIPList.push(ip);
+    app.data.serverButtonCount++;
+  }
   setTimeout(() => {
-    serverButtonDenyIPList.splice(serverButtonDenyIPList.indexOf(ip), 1);
+    const index = serverButtonDenyIPList.indexOf(ip);
+    if (index !== -1) serverButtonDenyIPList.splice(index, 1);
     res.redirect('/serverbutton/');
   }, 1000);
 });
